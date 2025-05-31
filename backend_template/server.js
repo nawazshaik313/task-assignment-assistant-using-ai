77a7412ca7ee57fcaa9a1a30139dadb0532f38b5
nawazshaik313 <nawazshaikmohammed313@gmail.com>
2025-05-31 20:42:36 +0530

    Removed cloud serves

--- backend_template/server.js
+++ backend_template/server.js
@@ -7,12 +7,29 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Import route files
 const taskRoutes = require("./routes/taskRoutes");
-app.use("/api/tasks", taskRoutes);
+const userRoutes = require("./routes/userRoutes");
+const assignmentRoutes = require("./routes/assignmentRoutes");
+const pendingUserRoutes = require("./routes/pendingUserRoutes");
+const programRoutes = require("./routes/programRoutes");
+const adminLogRoutes = require("./routes/adminLogRoutes");
+const currentUserRoutes = require("./routes/currentUserRoutes");
 
+// Use routes
+app.use("/api/tasks", taskRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/assignments", assignmentRoutes);
+app.use("/api/pending-users", pendingUserRoutes);
+app.use("/api/programs", programRoutes);
+app.use("/api/admin-logs", adminLogRoutes);
+app.use("/api/current-user", currentUserRoutes);
+
+// Connect DB and start server
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  
