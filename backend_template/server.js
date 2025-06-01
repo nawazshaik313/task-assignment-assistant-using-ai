862cb38c17a70f08a164e0f50709c093bb19cc39
nawazshaik313 <nawazshaikmohammed313@gmail.com>
2025-06-01 12:35:26 +0530

    Removed cloud serves3

--- backend_template/server.js
+++ backend_template/server.js
@@ -28,3 +28,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => {
+res.send('Backend API is running...');
+});
